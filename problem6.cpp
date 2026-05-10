d6c63e34d9a1b23b0cbce56ee305a9396fa18a75
Xavanium <xavanium@gmail.com>
2026-05-10 19:04:41 +0000

    solved problem 6

---
+++ problem6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+//actually solved this with desmos lol
+//used sigma notation and remembered the sum of natural numbers 1-100 (5050)
+int main(){
+    int sum1 = 5050;
+    int sum2 = 338350;
+    int result = pow(sum1, 2) - sum2;
+    std::cout << result << std::endl;
+}
